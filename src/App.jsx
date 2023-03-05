530e654d18a3b482fa7e6f52b36abcac1a5a026b
anna <b10332040@gmail.com>
2023-03-05 10:59:36 +0800

    U66-U75實作

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages';
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="todo" element={<TodoPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
